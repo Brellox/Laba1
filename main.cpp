e79206425a401f80c03d7f3bc6e04b049a863dc2
Ivan Suslov <vanchous3000@gmail.com>
2019-10-17 15:02:39 +0700

    немного подрихтовал интерфейс,разобраться с присваиванием, допилить интерфейс

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ vector <Payment> rabotyaga;
 int menu()
 {
     int a;
-    cout << "1 - вычислить месячный оклад работяги" << "\n";
-    cout << "2 - узнать сколько работяга отдает в пфр" << "\n";
-    cout << "3 - узнать сколько нологе платит работяга" << "\n";
-    cout << "4 - вывести данные работяги" << "\n";
+    cout << "1 - добавить работягу" << "\n";
+    cout << "2 - прибавить работяге к окладу" << "\n";
+    cout << "3 - сменить год поступления работяги" << "\n";
+    cout << "4 - рассчитать зп работяги" << "\n";
     cin >> a;
     return a;
     
@@ -31,91 +31,35 @@ void addRabotyaga ()
     delete [] creds;
 }
 
-/*void changeInfo()
-{
-    char* creds = new char [64];
-    double salary;
-    int year, days, a;
-    cout << "Введите имечко фамилию" << "\t";
-    cin.getline(creds,64);
-    cout << "Введите дневной оклад" << "\t";
-    cin >> salary;
-    cout << "Введите в каком году работяга поступил на завод" << "\t";
-    cin >> year;
-    cout << "Введите сколько дней в месяце отработал работяга" << "\t";
-    cin >> days;
-    cout << "Введите номер работяги" << "\t";
-    cin >> a;
-    Payment rabotyaga1(creds,salary,year,days);
-    rabotyaga[a] = rabotyaga1;
-    delete [] creds;
-}
-*/
-
 int main()
 {
     setlocale(LC_ALL,"Russian");
-    
-   /* Payment rabotyaga1;
-    Payment rabotyaga2 ("Vasya Pupkin",3215.67, 2007, 22);
-    Payment rabotyaga3 = rabotyaga2;
-    Payment rabotyaga4(rabotyaga3);
-    
-    rabotyaga3.setCreds("Pupa Lupin");
-    rabotyaga3.setSalary(3551.64);
-    rabotyaga3.setYear(2013);
-    rabotyaga3.setDays(19);
-     
     for(;;)
     {
         switch (menu()) {
             case 1:
             {
-                cout << rabotyaga1.salaryCount() << "\n";
-                cout << rabotyaga2.salaryCount() << "\n";
-                cout << rabotyaga3.salaryCount() << "\n";
+                addRabotyaga();
                 break;
                 
             }
             case 2:
             {
-             cout << rabotyaga1.pension() << "\n";
-             cout << rabotyaga2.pension() << "\n";
-             cout << rabotyaga3.pension() << "\n";
+             
              break;
             }
             case 3:
             {
-            cout << rabotyaga1.plotiNologe() << "\n";
-            cout << rabotyaga2.plotiNologe() << "\n";
-            cout << rabotyaga3.plotiNologe() << "\n";
+            
                 break;
             }
             case 4:
             {
-             cout << rabotyaga1.toString() << "\n";
-             cout << rabotyaga2.toString() << "\n";
-             cout << rabotyaga3.toString() << "\n";
+             
              break;
             }
             default:
                 return 0;
         }
     }
-        
-    /*
-    
-    cout << rabotyaga3.getCreds() << "\n";
-    cout << rabotyaga3.getSalary() << "\n";
-    cout << rabotyaga3.getYear() << "\n";
-    cout << rabotyaga3.getDays() << "\n";
-    
-    cout <<"------------------------------" << "\n";
-    
-    cout << rabotyaga2.salaryCount() << "\n";
-    cout << rabotyaga2.pension() << "\n";
-    cout << rabotyaga2.plotiNologe() << "\n";
-    cout << rabotyaga2.toString() << "\n";
-    cout << rabotyaga3.toString() << "\n";*/
-    
 }
