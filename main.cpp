3f2d086a7452068be7b9a009c86ef8e5b92b7778
Ivan Suslov <vanchous3000@gmail.com>
2019-10-24 01:09:08 +0700

    Проблемы с функцией cin.getline

--- main.cpp
+++ main.cpp
@@ -1,24 +1,126 @@
 #include "Header.h"
 
 vector <Payment> rabotyaga;
+
 int menu()
 {
     int a;
     cout << "1 - добавить работягу" << "\n";
-    cout << "2 - прибавить работяге к окладу" << "\n";
-    cout << "3 - сменить год поступления работяги" << "\n";
-    cout << "4 - рассчитать зп работяги" << "\n";
+    cout << "2 - изменить данные работяги" << "\n";
+    cout << "3 - изменить оклад работяги" << "\n";
+    cout << "4 - изменить год поступления работяги" << "\n";
+    cout << "5 - рассчитать зп работяги" << "\n";
+    cout << "6 - вывести данные работяги" << "\n";
+    cout << "9 - просто показать что оно живое" << "\n";
     cin >> a;
     return a;
     
 }
+
+void test ()
+{
+    double diff = 12.34;
+    rabotyaga[0] = rabotyaga[1] + diff;
+    rabotyaga[1] = rabotyaga[2] - diff;
+}
+
+
+void out()
+{
+    int n;
+    cout << "Введите номер работяги которого хотите вывести:" << "\t";
+    cin >> n;
+    rabotyaga[n].toString();
+}
+
+void countSalary()
+{
+    int n;
+    double t;
+    cout << "Введите номер работяги, зарплату которого хотите рассчитать:" << "\t";
+    cin >> n;
+    t = rabotyaga[n]();
+    cout << "Зарплата с учетом вычета всех процентов составит: " << t << "\n";
+}
+
+void changeYear()
+{
+    int n, a;
+    cout << "Введите номер работяги, год поступления которого хотите изменить:" << "\t";
+    cin >> n;
+    cout << "1 - повысить год поступления" << "\n";
+    cout << "2 - понизить год поступления" << "\n";
+    cin >> a;
+    switch (a)
+    {
+        case 1:
+        {
+            rabotyaga[n]++;
+            break;
+            
+        }
+        case 2:
+        {
+            rabotyaga[n]--;
+         break;
+        }
+    }
+}
+
+void changeSalary()
+{
+    int n, a;
+    double dif;
+    cout << "Введите номер работяги, оклад которого хотите изменить:" << "\t";
+    cin >> n;
+    cout << "Введите насколько вы хотите изменить размер оклада:" << "\t";
+    cin >> dif;
+    cout << "1 - повысить оклад" << "\n";
+    cout << "2 - понизить оклад" << "\n";
+    cin >> a;
+    switch (a)
+    {
+        case 1:
+        {
+            rabotyaga[n]+=dif;;
+            break;
+            
+        }
+        case 2:
+        {
+            rabotyaga[n]-=dif;
+         break;
+        }
+    }
+}
+
+void changeInfo()
+{
+    char* creds = new char [64];
+    double salary;
+    int year, days, n;
+    cout << "Введите имечко фамилию" << "\t";
+    cin.getline(creds,64);
+    cout << "Введите дневной оклад" << "\t";
+    cin >> salary;
+    cout << "Введите в каком году работяга поступил на завод" << "\t";
+    cin >> year;
+    cout << "Введите сколько дней в месяце отработал работяга" << "\t";
+    cin >> days;
+    Payment rabotyaga1(creds,salary,year,days);
+    cout << "Введите номер работяги, данные которого хотите изменить:" << "\t";
+    cin >> n;
+    rabotyaga[n] = rabotyaga1;
+    delete [] creds;
+}
+
 
 void addRabotyaga ()
 {
     char* creds = new char [64];
     double salary;
     int year, days;
-    cout << "Введите имечко фамилию" << "\t";
+    cout << "Введите имечко фамилию" << "\n";
     cin.getline(creds,64);
     cout << "Введите дневной оклад" << "\t";
     cin >> salary;
@@ -36,27 +138,46 @@ int main()
     setlocale(LC_ALL,"Russian");
     for(;;)
     {
-        switch (menu()) {
+        switch (menu())
+        {
             case 1:
             {
                 addRabotyaga();
                 break;
                 
             }
+                
             case 2:
             {
-             
-             break;
-            }
-            case 3:
-            {
-            
+                changeInfo();
                 break;
             }
+                
+            case 3:
+            {
+                changeSalary();
+                break;
+            }
+                
             case 4:
             {
-             
-             break;
+                changeYear();
+                break;
+            }
+            case 5:
+            {
+                countSalary();
+                break;
+            }
+            case 6:
+            {
+                out();
+                break;
+            }
+            case 9:
+            {
+                test();
+                break;
             }
             default:
                 return 0;
