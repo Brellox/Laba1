5d167af23dacafac5d15c673caff467a1862e4d8
Ivan Suslov <vanchous3000@gmail.com>
2019-11-09 12:50:27 +0700

    ошибка с текстовым файлом исправлена

--- Header.cpp
+++ Header.cpp
@@ -1,3 +1,4 @@
+
 #include "Header.h"
 //laba1
 
@@ -173,29 +174,29 @@ double Payment::operator() ()
 
 ostream& operator<<(ostream& out, const Payment& rabotyaga)
 {
-    out << rabotyaga.creds << "\n" << rabotyaga.salary << "\n" << rabotyaga.year << "\n" << rabotyaga.days << "\n";
+    out << rabotyaga.salary << " " << rabotyaga.year << " " << rabotyaga.days << " " << rabotyaga.creds << "\n";
     return out;
 }
 
 istream& operator>>(istream& in, Payment& rabotyaga)
 {
-    char* temp = new char[64];
-    in.getline (temp, 63);
-    rabotyaga.setCreds(temp);
     in >> rabotyaga.salary;
     in >> rabotyaga.year;
     in >> rabotyaga.days;
+    char* temp = new char[64];
+    in.getline (temp, 63);
+    rabotyaga.setCreds(temp);
     return in;
 }
 
 ifstream& operator>>(ifstream& in, Payment& rabotyaga)
 {
-    char* temp = new char[64];
-    in.getline (temp, 63);
-    rabotyaga.setCreds(temp);
     in >> rabotyaga.salary;
     in >> rabotyaga.year;
     in >> rabotyaga.days;
+    char* temp = new char[64];
+    in.getline (temp, 63);
+    rabotyaga.setCreds(temp);
     return in;
 }
 
